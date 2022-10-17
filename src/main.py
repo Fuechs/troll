69cf70dd060f1e49c919fe18ac2f7c67c3ad5c97
Füchs <68223337+Fuechs@users.noreply.github.com>
2022-10-17 20:03:57 +0200

    bytecode generation finished

--- src/main.py
+++ src/main.py
@@ -8,9 +8,11 @@
 
 from subprocess import call
 from sys import argv
+from bytecode import Optimizer, Generator, debugBytecode
 from error import TrollResult
 from lexer import Lexer, Token
 from parser import Parser, debugAST
+from pickle import dump
 
 clear = lambda: call("clear")
     
@@ -35,7 +37,29 @@ def main(argc: int, argv: list[str]) -> TrollResult:
     if res.success is False:
         return res
     
-    debugAST(ast)
+    # debugAST(ast)
+    
+    # nothing to optimize here yet
+    
+    # optimizer = Optimizer(ast)
+    # ast, res = optimizer.optimize()
+    
+    # if res.success is False:
+    #     return res
+    
+    # debugAST(ast)
+    
+    generator = Generator(ast)
+    code, res = generator.generate()
+    
+    debugBytecode(code)
+        
+    with open("out.bin", "wb") as outb:
+        dump(code, outb)
+    
+    if res.success is False:
+        return res
+    
     
     # interpret ...
     
